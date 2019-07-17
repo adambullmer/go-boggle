4cc2b2bdafdbd0a10351bbad728923ed4fb1e556
Adam Bullmer <psycodrumfreak@gmail.com>
2019-07-16 23:58:27 -0500

    Add benchmarks for CheckWord

--- internal/lexicon/lexicon_test.go
+++ internal/lexicon/lexicon_test.go
@@ -10,3 +10,43 @@ func BenchmarkLexicon(b *testing.B) {
 		lex.LoadLexicon("../../dictionaries/sowpods.txt")
 	}
 }
+
+func BenchmarkCheckWordShort(b *testing.B) {
+	lex := Lexicon{}
+	lex.LoadLexicon("../../dictionaries/sowpods.txt")
+	for i := 0; i < b.N; i++ {
+		lex.CheckWord("sin")
+	}
+}
+
+func BenchmarkCheckWordMedium(b *testing.B) {
+	lex := Lexicon{}
+	lex.LoadLexicon("../../dictionaries/sowpods.txt")
+	for i := 0; i < b.N; i++ {
+		lex.CheckWord("since")
+	}
+}
+
+func BenchmarkCheckWordLong(b *testing.B) {
+	lex := Lexicon{}
+	lex.LoadLexicon("../../dictionaries/sowpods.txt")
+	for i := 0; i < b.N; i++ {
+		lex.CheckWord("science")
+	}
+}
+
+func BenchmarkCheckWordExtraLong(b *testing.B) {
+	lex := Lexicon{}
+	lex.LoadLexicon("../../dictionaries/sowpods.txt")
+	for i := 0; i < b.N; i++ {
+		lex.CheckWord("scientifically")
+	}
+}
+
+func BenchmarkCheckWordMissing(b *testing.B) {
+	lex := Lexicon{}
+	lex.LoadLexicon("../../dictionaries/sowpods.txt")
+	for i := 0; i < b.N; i++ {
+		lex.CheckWord("foobarbaz")
+	}
+}
